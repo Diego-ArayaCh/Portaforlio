0bff20bcb876cc44e4a1abdee99b753df47a1f1a
DiegoArayaCh-dev <juandiegoa907@gmail.com>
2022-05-02 20:46:47 -0600

    Final dist

--- Frontend/Admin/src/app/admin/Projects/projects-details/projects-details.component.ts
+++ Frontend/Admin/src/app/admin/Projects/projects-details/projects-details.component.ts
@@ -14,7 +14,7 @@ export class ProjectsDetailsComponent implements OnInit {
   constructor( private _projectService: ProjectService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    
+    this.id = this.route.snapshot.paramMap.get('id');
     this.loadContent();
     
     
