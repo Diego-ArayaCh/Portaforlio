6305e806f2f3dca1bd8536c12d8b256cdb050103
DiegoArayaCh-dev <juandiegoa907@gmail.com>
2022-05-03 02:03:59 -0600

    chart.js added

--- Frontend/Admin/src/app/admin/start/start.component.ts
+++ Frontend/Admin/src/app/admin/start/start.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+import { ProjectService } from 'src/app/services/project.service';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-start',
@@ -6,16 +9,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start.component.css']
 })
 export class StartComponent implements OnInit {
+  @ViewChild('mychart') mychart: any;
+  canvas: any;
+  ctx: any;
+  numProjects: any;
+  numVisits: any;
 
-  constructor() { }
+
+  constructor(private _projectService: ProjectService) { }
 
   ngOnInit(): void {
+    this.loadCards()
   }
-  ckeditorContent = "<h1 style='align: center' >Hello</h1>";
 
-save(){
-  document.getElementById('Project')!.innerHTML = this.ckeditorContent;
+  loadCards() {
+   
+    
 
-}
+    this._projectService.get().subscribe({
+      next: async(data) => {
+        this.numProjects = data.length;
+        
+        // if (data) {
+        //   for (let index = 0; index < data.length; index++) {
+        //     const element = data[index];
+        //     array.push(element);
+            
+        //   }
+        // }
+      
+        
+      },
+      error(err) { console.log('Received an error: ' + err)}
+    });
+  
+      this.numVisits = 900
+  
+   
+  }
+  ngAfterViewInit() {
+    //Variables para el grafico de la parte superior del dashboard
+    this.canvas = this.mychart.nativeElement;
+    this.ctx = this.canvas.getContext('2d');
+
+    //Se debe remplazar con las ganancias de 12 meses
+    var dataDB = [150, 150, 300, 475, 700, 689, 720, 150, 900, 300, 475, 340];
+    const myChart = new Chart(this.ctx, {
+      type: 'line',
+      data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+          datasets: [{
+              label: '# of Visits',
+              data: dataDB,
+              borderColor: '#FC8019',
+              tension: 0.1,
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+            myScale: {
+              type: 'logarithmic',
+              position: 'left', // `axis` is determined by the position as `'y'`
+            }
+           
+          }
+      }
+  });
+  }
+
+
 
 }
